616ae650ef19dfe557c2c37020339eeb1ee49708
Camilo O. <onahuel2002@outlook.com>
2026-03-10 13:12:44 -0300

    Redesign widget shell UI

--- livepro-connector/livepro-connector.php
+++ livepro-connector/livepro-connector.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: LivePro Connector
  * Description: Conecta WooCommerce con el backoffice de LivePro e inyecta el widget de live shopping.
- * Version: 0.3.0
+ * Version: 0.4.0
  * Author: LivePro
  */
 
