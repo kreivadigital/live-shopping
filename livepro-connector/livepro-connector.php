5151b748d2501b2a2a7654ccfd907061afd44b59
Camilo O. <onahuel2002@outlook.com>
2026-03-10 13:34:47 -0300

    Add product bottom sheet

--- livepro-connector/livepro-connector.php
+++ livepro-connector/livepro-connector.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: LivePro Connector
  * Description: Conecta WooCommerce con el backoffice de LivePro e inyecta el widget de live shopping.
- * Version: 0.4.0
+ * Version: 0.5.0
  * Author: LivePro
  */
 
