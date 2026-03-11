484c303ed8e70b46cdcf6667bf3cd25bf9574280
Camilo O. <onahuel2002@outlook.com>
2026-03-11 00:59:58 -0300

    Bump plugin version to 0.8.0

--- livepro-connector/livepro-connector.php
+++ livepro-connector/livepro-connector.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: LivePro Connector
  * Description: Conecta WooCommerce con el backoffice de LivePro e inyecta el widget de live shopping.
- * Version: 0.7.0
+ * Version: 0.8.0
  * Author: LivePro
  */
 
